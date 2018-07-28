536af6be61e0181a862b71fb1c4a17bcdc690554
Michael Palhidai <mpalhidai@gmail.com>
2018-07-27 20:59:38 -0700

    everything works with the devise defaults now

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_scope :user do
-    post 'users/sign_up' => 'devise/registrations#create'
-  end
   root 'static_pages#home'
   resources :users, :only => [:index]
 
