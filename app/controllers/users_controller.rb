2320e935f8733972a25c003bd18647a5791c7775
Michael Palhidai <mpalhidai@gmail.com>
2018-07-27 17:29:16 -0700

    fixed routes to signup user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
     @users = User.all
     @user = current_user
   end
+
+  # private
+  #
+  # def user_params
+  #   params.require(:user).permit(:first_name, :last_name, :email, :birthday)
+  # end
+  #
+  # def user
+  #   @user ||= User.find(params[:id])
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    post 'users/sign_up' => 'devise/registrations#create'
+  end
   root 'static_pages#home'
   resources :users, :only => [:index]
 
