c8d3f1bb7ff8b4f97331c0a139cf3f7e2d606602
Michael Palhidai <mpalhidai@gmail.com>
2018-08-13 17:02:11 -0700

    omniauth with 5.2 enc credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: %i[facebook]
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:facebook]
 
   after_create :create_profile
 
@@ -31,12 +31,23 @@ class User < ApplicationRecord
     Post.where("user_id IN (#{i_approved}) OR user_id IN (#{you_approved}) OR user_id = :user_id", user_id: self.id)
   end
 
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      user.first_name = auth.info.first_name
-      user.last_name = auth.info.last_name
+      user.first_name = auth.info.name.split[0]
+      user.last_name = auth.info.name.split[1]
+      # user.profile.gender = auth.extra.raw_info.gender
+      # user.profile.location = auth.info.location[name]
+      # user.profile.avatar = auth.info.image
       # user.birthday = auth.info.birthday
       # user.image = auth.info.image # figure out how to get image to profile :avatar
       # If you are using confirmable and the provider(s) you use validate emails,
